edb52360eb203fc843c6516d61f18bb8d015e467
Nisanur Türkmen <025nisanur@gmail.com>
2026-09-13 17:04:22 +0300

    feat(ForgotPasswordFragment): Add UI layout and back to login navigation

--- app/src/main/java/com/example/food_app/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/food_app/ForgotPasswordFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.FragmentContainer
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +39,19 @@ class ForgotPasswordFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forgot_password, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Giriş Ekranına Dön
+        val btnBackToLogin = view.findViewById<TextView>(R.id.btn_back_to_login)
+        btnBackToLogin.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, LoginFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
 
     companion object {
         /**
